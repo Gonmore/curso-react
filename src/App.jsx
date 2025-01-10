68136d9ad259a9c0da51c189b57ec93780ee95ef
Gonmore <armandito3.16@gmail.com>
2025-01-09 21:11:32 -0400

    agregando variables de estado

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Home from './screens/home'
 
 
 import './App.css'
+import { Counter } from './components/counter/counter'
 
 function App() {
 
   return (
     <>
       <Home />
+      <Counter/>
     </>
   )
 }
