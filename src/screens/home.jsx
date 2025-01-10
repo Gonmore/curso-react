9cdf827ebfc29ac5e65eda2edb6266303cdc87ea
Gonmore <armandito3.16@gmail.com>
2025-01-09 21:41:07 -0400

    Labo 7

--- src/screens/home.jsx
+++ src/screens/home.jsx
@@ -2,6 +2,7 @@
 import Body from '../components/home/body.jsx';
 import Footer from '../components/home/footer.jsx';
 import OpenLink from '../components/core/OpenLink.jsx';
+import { useSelector, useDispatch } from 'react-redux'
 
 import viteLogo from '../assets/vite.svg';
 import reactLogo from '../assets/react.svg';
@@ -9,9 +10,13 @@ import githubLogo from '../assets/git.jpg';
 
 
 
+
 const Home = () => {
+    const count = useSelector((state) => state.counter.value)
+    const dispatch = useDispatch()
     return (
         <div>
+            <span>Value: {count}</span>
             <div>
                 <OpenLink
                     redirectURL="https://vite.dev"
